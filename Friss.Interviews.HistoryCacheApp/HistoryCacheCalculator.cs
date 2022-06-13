eaef8ed650c1ed1983519de4ba519238998fe48e
Hermógenes Ferreira <hermogenes.ferreira@friss.com>
2022-06-13 13:41:02 +0200

    add items

--- Friss.Interviews.HistoryCacheApp/HistoryCacheCalculator.cs
+++ Friss.Interviews.HistoryCacheApp/HistoryCacheCalculator.cs
@@ -1,5 +1,19 @@
 namespace Friss.Interviews.HistoryCacheApp;
 
+/*
+At FRISS we have an application where users can search for entities (e.g. person, companies, vehicles)
+The users now want to know the last 5 *distinct* terms they searched, *ordered* from the most recent to the last
+
+Example 1:
+
+terms: "FRISS B.V.", "Hermes", "Tesla", "Netherlands", "Germany"
+
+result: ["Germany", "Netherlands", "Tesla", "Hermes", "FRISS B.V"]
+
+Example 2:
+
+terms: ["FRISS B.V", "Hermes", "FRISS B.V"]
+*/
 public class HistoryCacheCalculator
 {
     public void Add(string term)
